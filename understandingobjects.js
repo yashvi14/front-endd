a0ae565002db3995e273b33098c8ed7ea1cb0a84
yashvi14 <51016637+yashvi14@users.noreply.github.com>
2019-06-09 12:19:13 +0530

    Add files via upload

---
+++ understandingobjects.js
@@ -0,0 +1,28 @@
+
+var human=
+{
+	firstname:"Ram",
+	lastname:"Sham",
+	age:0,
+
+fullname:function()
+{
+	return this.firstname+this.lastname
+
+}
+
+
+}
+
+
+function batsman(name,strikeratio,runscore){
+
+this.name=name;
+this.strikeratio=strikeratio;
+this.runscore=runscore;
+}
+console.log(batsman);
+var person=new Object();
+person.age="21";
+person.height="4.9";
+var person
