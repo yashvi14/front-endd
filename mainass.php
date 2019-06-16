8686d45e84b2658473f7a0118e9a1bf6114b0132
yashvi14 <51016637+yashvi14@users.noreply.github.com>
2019-06-17 01:05:27 +0530

    Add files via upload

---
+++ mainass.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!-- Favicon icon -->
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Material Pro Admin Template - The Most Complete & Trusted Bootstrap 4 Admin Template</title>
+    <!-- Bootstrap Core CSS -->
+    <link href="../assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- You can change the theme colors from here -->
+    <link href="css/colors/blue.css" id="theme" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body>
+<aside class="left-sidebar">
+            <!-- Sidebar scroll-->
+            <div class="scroll-sidebar">
+                <!-- User profile -->
+                <div class="user-profile" style="background: url(../assets/images/background/user-info.jpg) no-repeat;">
+                    <!-- User profile image -->
+                    <div class="profile-img"> <img src="../assets/images/users/profile.png"  alt="user" /> </div>
+                    <!-- User profile text-->
+                    <div class="profile-text"> <a role="button" aria-haspopup="true" aria-expanded="true">YASHVI SHAH</a>
+                        <div class="dropdown-menu animated flipInY">
+                            <a href="#" class="dropdown-item"><i class="ti-user"></i> My Profile</a>
+                            <a href="#" class="dropdown-item"><i class="ti-wallet"></i> My Balance</a>
+                            <a href="#" class="dropdown-item"><i class="ti-email"></i> Inbox</a>
+                            <div class="dropdown-divider"></div> <a href="#" class="dropdown-item"><i class="ti-settings"></i> Account Setting</a>
+                            <div class="dropdown-divider"></div> <a href="login.html" class="dropdown-item"><i class="fa fa-power-off"></i> Logout</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- End User profile text-->
+                <!-- Sidebar navigation-->
+                <nav class="sidebar-nav">
+                    <ul id="sidebarnav">
+                        <li class="nav-small-cap">PERSONAL</li>
+                        <li>
+                            <a aria-expanded="false"><span class="hide-menu">DASHBOARD </span></a>
+                           
+                        </li>
+
+                        <li> <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><span class="hide-menu">SCHOOL</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <div class="carousel-item">
+                                                    <div class="container"><img class="d-block img-fluid" src="../assets/images/big/img3.jpg" alt="Third slide"></div>
+                                                </div>
+                                                
+                               <!-- <div><i class="mdi mdi-chair-school"></i><code></code></div>
+                               -->
+                                <li><a href="school.php">View/Edit</a></li>
+                                <li><a href="">Add</a></li>
+                                <li><a href="">Moderate</a></li>
+                                <li><a href="">Queries(Support)</a></li>
+                                <li><a href="Boards.php">Boards</a></li>
+                                <li><a href="">Facilities</a></li>
+                                <li><a href="">Add Field</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><span class="hide-menu">PARENT</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="par.php">View/Edit</a></li>
+                                <li><a href="">Moderations</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><span class="hide-menu">CHILD</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="par.php">View/Edit</a></li>
+                                <li><a href="">Moderations</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><span class="hide-menu">SUPPORT</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="">School</a></li>
+                                <li><a href="">Parent</a></li>
+                                <li><a href="">Child</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark" aria-expanded="false"><span class="hide-menu">FAQ</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="table2.php">View/Edit</a></li>
+                                <li><a href="">Add</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a class="has-arrow waves-effect waves-dark"  aria-expanded="false" href="table2.php"><span class="hide-menu">TEAM MEMBERS</span></a>
+                            <ul aria-expanded="false" class="collapse">
+                                <li><a href="team.php">View/Edit</a></li>
+                                <li><a href="">Add</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- End Sidebar navigation -->
+            </div>
+            <!-- End Sidebar scroll-->
+            <!-- Bottom points-->
+            <div class="sidebar-footer">
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Settings"><i class="ti-settings"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Email"><i class="mdi mdi-gmail"></i></a>
+                <!-- item-->
+                <a href="" class="link" data-toggle="tooltip" title="Logout"><i class="mdi mdi-power"></i></a>
+            </div>
+            <!-- End Bottom points-->
+        </aside>
+         <script src="../assets/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="../assets/plugins/bootstrap/js/tether.min.js"></script>
+    <script src="../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Wave Effects -->
+    <script src="js/waves.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="../assets/plugins/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <script src="../assets/plugins/sparkline/jquery.sparkline.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/custom.min.js"></script>
+    <!-- ============================================================== -->
+    <!-- Style switcher -->
+    <!-- ============================================================== -->
+    <script src="../assets/plugins/styleswitcher/jQuery.style.switcher.js"></script>
+    </body>
